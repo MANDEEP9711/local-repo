6b405d40998545c73a292283ddeb99c33ed36849
MANDEEP9711 <mandeepsingh971159@gmail.com>
2025-06-17 11:11:05 +0530

    change by mandeep

--- analog-clock/src/App.jsx
+++ analog-clock/src/App.jsx
@@ -11,9 +11,9 @@ function App() {
   const hours = time.getHours();
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
-  const hourDeg = (hours % 12) * 30 + minutes * 0.5 - 90; // 360 / 12 = 30 degrees per hour, plus minute adjustment
-  const minuteDeg = minutes * 6 + seconds * 0.1 - 90; // 360 / 60 = 6 degrees per minute, plus second adjustment
-  const secondDeg = seconds * 6 - 90; // 360 / 60 = 6 degrees per second
+  const hourDeg = (hours % 12) * 30 + minutes * 0.5 - 90;
+  const minuteDeg = minutes * 6 + seconds * 0.1 - 90; 
+  const secondDeg = seconds * 6 - 90; 
   const clockStyle = {
     transform: `rotate(${hourDeg}deg)`,
   };
@@ -35,9 +35,9 @@ function App() {
     <div 
     className='bg-cover bg-center h-screen flex flex-col items-center justify-center'
     style={{ backgroundImage: `url(${logo})` }}>
-      <h1 className="text-center text-2xl font-bold mt-4 text-amber-50">Analog Clock</h1>
+      <h1 className="text-center text-8xl font-bold mt-4 text-amber-50">Analog Clock</h1>
       <div className="clock-container flex justify-center items-center h-screen">
-        <div className="clock relative w-70 h-70 rounded-full border-8 border-black flex items-center justify-center bg-red-200">
+        <div className="clock relative w-80 h-80 rounded-full border-35 border-white flex items-center justify-center bg-red-200">
           {/* Clock numbers */}
           {clockNumbers.map((num, i) => {
             const angle = i * 30; // 360 / 12
@@ -46,8 +46,8 @@ function App() {
                 key={num}
                 className="number absolute text-gray-700 font-bold text-xl"
                 style={{
-                  transform: `rotate(${angle}deg) translateY(-110px) rotate(-${angle}deg)`,
-                  //transformOrigin: 'center center',
+                  transform: `rotate(${angle}deg) translateY(-140px) rotate(-${angle}deg)`,
+                  transformOrigin: 'center center',
                 }}
               >
                 {num}
@@ -56,10 +56,12 @@ function App() {
           })}
 
           {/* Clock hands */}
-          <div className='absolute hand hour-hand bg-gray-600 w-25 h-2 top-[50%] left-[50%]' style={clockStyle}></div>
-          <div className='absolute hand minute-hand bg-gray-600 w-24 h-1.5 top-[50%] left-[50%]' style={minuteStyle}></div>
-          <div className='absolute hand second-hand bg-red-500 w-28 h-1 top-[50%] left-[50%]' style={secondStyle}></div>
-          <div className='absolute text-center top-[50%] left-[20%] w-15 h-12 rounded-xl bg-green-100'>{time.toLocaleTimeString()}</div>
+          <div className='absolute hand hour-hand bg-gray-600 w-30 h-2 top-[50%] left-[50%]' style={clockStyle}></div>
+          <div className='absolute hand minute-hand bg-gray-600 w-32 h-1.5 top-[50%] left-[50%]' style={minuteStyle}></div>
+          <div className='absolute hand second-hand bg-red-500 w-31 h-1 top-[50%] left-[50%]' style={secondStyle}></div>
+          <div className='w-5 h-5 bg-white rounded-4xl z-10'></div>
+          <div className='absolute bg-gray-500 z-30 w-2 h-2 rounded-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'></div>
+          <div className='absolute text-center top-[50%] left-[5%] w-15 h-12 rounded-xl bg-green-100'>{time.toLocaleTimeString()}</div>
        </div>
       </div>
 
